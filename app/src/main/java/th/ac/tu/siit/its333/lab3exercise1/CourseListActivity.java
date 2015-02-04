4cd4a7b97a3df99b555b348f1bae925d26126d51
sevenups <sakexev1b@gmail.com>
2015-02-04 11:45:08 +0700

    Finish eiei

--- app/src/main/java/th/ac/tu/siit/its333/lab3exercise1/CourseListActivity.java
+++ app/src/main/java/th/ac/tu/siit/its333/lab3exercise1/CourseListActivity.java
@@ -1,9 +1,11 @@
 package th.ac.tu.siit.its333.lab3exercise1;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.*;
 
 
 public class CourseListActivity extends ActionBarActivity {
@@ -12,6 +14,30 @@ public class CourseListActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_list);
+
+        TextView tvList = (TextView)findViewById(R.id.tvList);
+
+        Intent i = getIntent();
+        String[] input = i.getStringArrayExtra("fromMA");
+
+        if(input.length == 0){
+
+            tvList.setText("No course to be displayed");
+
+        }else {
+
+            String output = "List of courses\n\n";
+
+            for (int index = 0; index < input.length; index++) {
+                output = output + input[index] + "\n";
+            }
+
+
+            tvList.setText(output);
+
+        }
+
+
     }
 
 
--- app/src/main/java/th/ac/tu/siit/its333/lab3exercise1/MainActivity.java
+++ app/src/main/java/th/ac/tu/siit/its333/lab3exercise1/MainActivity.java
@@ -1,14 +1,16 @@
 package th.ac.tu.siit.its333.lab3exercise1;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.text.*;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,11 +37,150 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void buttonClicked(View v) {
+
+        int id = v.getId();
+
+        switch(id){
+            case R.id.button:
+                listCodes = new ArrayList<String>();
+                listCredits = new ArrayList<Integer>();
+                listGrades = new ArrayList<String>();
+                calculateGPA();
+                break;
+            case R.id.button2:
+                Intent i = new Intent(this,CourseActivity.class);
+                startActivityForResult(i, 99);
+                break;
+            case R.id.button4:
+                Intent x = new Intent(this,CourseListActivity.class);
+                String[] courseListToBeDisplayed = new String[listCodes.size()];
+
+
+                for(int index = 0 ; index < listCodes.size() ; index++){
+
+                    switch(Integer.parseInt(listGrades.get(index))){
+                        case R.id.rbA:
+                            courseListToBeDisplayed[index] = listCodes.get(index) + " (" + listCredits.get(index) + " credits) = " + "A";
+                            break;
+                        case R.id.rbBP:
+                            courseListToBeDisplayed[index] = listCodes.get(index) + " (" + listCredits.get(index) + " credits) = " + "B+";
+                            break;
+                        case R.id.rbB:
+                            courseListToBeDisplayed[index] = listCodes.get(index) + " (" + listCredits.get(index) + " credits) = " + "B";
+                            break;
+                        case R.id.rbCP:
+                            courseListToBeDisplayed[index] = listCodes.get(index) + " (" + listCredits.get(index) + " credits) = " + "C+";
+                            break;
+                        case R.id.rbC:
+                            courseListToBeDisplayed[index] = listCodes.get(index) + " (" + listCredits.get(index) + " credits) = " + "C";
+                            break;
+                        case R.id.rbDP:
+                            courseListToBeDisplayed[index] = listCodes.get(index) + " (" + listCredits.get(index) + " credits) = " + "D+";
+                            break;
+                        case R.id.rbD:
+                            courseListToBeDisplayed[index] = listCodes.get(index) + " (" + listCredits.get(index) + " credits) = " + "D";
+                            break;
+                        case R.id.rbF:
+                            courseListToBeDisplayed[index] = listCodes.get(index) + " (" + listCredits.get(index) + " credits) = " + "F";
+                            break;
+
+                    }
+
+                }
+
+                    x.putExtra("fromMA", courseListToBeDisplayed);
+
+                startActivity(x);
+                break;
+            default:
+                break;
+        }
+
+
+
+    }
+
+
+    public void calculateGPA(){
+
+        double gp = 0;
+        int credit = 0;
+        double gpa = 0;
+
+        for(int index = 0 ; index < listCodes.size() ; index++){
+
+
+            credit = credit + listCredits.get(index);
+
+            switch(Integer.parseInt(listGrades.get(index))){
+                case R.id.rbA:
+                    gp = gp + (4.0*listCredits.get(index));
+                    break;
+                case R.id.rbBP:
+                    gp = gp + (3.5*listCredits.get(index));
+                    break;
+                case R.id.rbB:
+                    gp = gp + (3.0*listCredits.get(index));
+                    break;
+                case R.id.rbCP:
+                    gp = gp + (2.5*listCredits.get(index));
+                    break;
+                case R.id.rbC:
+                    gp = gp + (2.0*listCredits.get(index));
+                    break;
+                case R.id.rbDP:
+                    gp = gp + (1.5*listCredits.get(index));
+                    break;
+                case R.id.rbD:
+                    gp = gp + (1.0*listCredits.get(index));
+                    break;
+                case R.id.rbF:
+                    gp = gp + (0*listCredits.get(index));
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        String GPA = "0";
+
+        if(credit != 0) {
+            gpa = gp / credit;
+
+            DecimalFormat df = new DecimalFormat("#.00");
+            GPA = df.format(gpa);
+        }
+
+
+
+        TextView tvCR = (TextView)findViewById(R.id.tvCR);
+        TextView tvGP = (TextView)findViewById(R.id.tvGP);
+        TextView tvGPA = (TextView)findViewById(R.id.tvGPA);
+
+        tvCR.setText(Integer.toString(credit));
+        tvGP.setText(Double.toString(gp));
+        tvGPA.setText(GPA);
+
+
+
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // Values from child activity
+
+        if(requestCode == 99){
+            if(resultCode == RESULT_OK){
+                String[] temp = data.getStringArrayExtra("fromCA");
+                listCodes.add(temp[0]);
+                listCredits.add(Integer.parseInt(temp[1]));
+                listGrades.add(temp[2]);
+
+                calculateGPA();
+            }
+        }
+
     }
 
     @Override
